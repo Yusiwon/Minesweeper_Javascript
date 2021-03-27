407351a58cd25434da63a946879472f789eb9b74
Yusiwon <danseryu@naver.com>
2021-03-27 15:59:22 +0900

    주변 지뢰 개수 세기까지

--- 지뢰찾기.js
+++ 지뢰찾기.js
@@ -1,14 +1,16 @@
+var tbody = document.querySelector('#table tbody');
+var dataset = [];
 document.querySelector('#exec').addEventListener('click', function () {
+    tbody.innerHTML = '';   // tbody의 내부를 모두 지우는 역할(= 내부 먼저 초기화)
     var col = parseInt(document.querySelector('#hor').value);
     var row = parseInt(document.querySelector('#ver').value);
     var mineCount = parseInt(document.querySelector('#mine').value);
-    console.log(hor, ver, mine);
 
     // 지뢰위치 뽑기
-    var mine = Array(row*col).fill()
+    var mine = Array(row * col).fill()
         .map(function (element, index) {
-        return index;
-    });
+            return index;
+        });
     var bumb = [];
     while (mine.length > 80) {
         var moveValue = mine.splice(Math.floor(Math.random() * mine.length), 1)[0];
@@ -16,20 +18,41 @@ document.querySelector('#exec').addEventListener('click', function () {
     }
 
     // 지뢰테이블 만들기
-    var dataset = [];
-    var tbody = document.querySelector('#table tbody');
     for (var i = 0; i < row; i += 1) {
         var arr = [];
         var tr = document.createElement('tr');
         dataset.push(arr);
         for (var j = 0; j < col; j += 1) {
-            arr.push(1);
+            arr.push(0);
             var td = document.createElement('td');
+            // td를 선언해주자마다 eventListner실행을 해준다.
+            // 해당 함수 밖에서 실행해주게 되면 변수td선언보다 리스너 실행이 먼저되므로
+            // 리스너가 빈 배열이 된다.
+            td.addEventListener('contextmenu', function (e) {
+                e.preventDefault();
+                var parentTr = e.currentTarget.parentNode; // contextmenu가 발생하는 태그
+                var parentTbody = e.currentTarget.parentNode.parentNode;
+                // 오른쪽 클릭시 해당 칸의 위치를 알아내기 위해
+                // Array.prototype은 배열이 아닌 유사배열의 위치를 알아내기 위할때 사용
+                var blank = Array.prototype.indexOf.call(parentTr.children, e.currentTarget);
+                var line = Array.prototype.indexOf.call(parentTbody.children, parentTr);
+
+                if (e.currentTarget.textContent === '' || e.currentTarget.textContent === 'X') {
+                    e.currentTarget.textContent = '!';
+                } else if (e.currentTarget.textContent === '!') {
+                    e.currentTarget.textContent = '?';
+                } else if (e.currentTarget.textContent === '?') {
+                    if (dataset[line][blank] === 0) {
+                        e.currentTarget.textContent = '';
+                    }else if (dataset[line][blank] === 'X') {
+                        e.currentTarget.textContent = 'X';
+                    }
+                }
+            });
             tr.appendChild(td);
         }
         tbody.appendChild(tr);
     }
-    console.log(dataset);
 
     // 지뢰심기기
     for (var k = 0; k < bumb.length; k += 1) { // 예 60 -> 7번째줄 0번째칸
@@ -40,4 +63,12 @@ document.querySelector('#exec').addEventListener('click', function () {
         tbody.children[bumbRow].children[bumbCol].textContent = 'X'; // 화면
         dataset[bumbRow][bumbCol] = 'X'; // 따로 관리하는 2차원배열
     }
-})
+});
+
+/* e.targer과 e.currenttarget의 차이점
+e.targer -> td(이벤트가 실제로 일어나는곳)
+e.currenttarger -> eventListener을 단 대상
+따라서 eventListener을 단 대상과 실제로 eventListener가 발생하는곳이 다를 수 있음
+ */
+
+
